0be21e5e59050d744ed40666f8ed13ebc410a2f0
thirteenfoil8 <thirteenfoil8@users.noreply.github.com>
2020-09-29 14:12:16 +0200

    change the implementation of ridge and least square

--- source/implementations.py
+++ source/implementations.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-def compute_loss(y, tx, w):
-    """Calculate the loss.
+def mse(error):
+    return 1/2*np.mean(error**2)
 
-    You can calculate the loss using mse or mae.
-    """
+def mae(error):
+    return np.mean(np.abs(error))
+
+def compute_mse(y, tx, w):
+    """Calculate the loss """
     error = y- tx.dot(w)
     return mse(error)
 
@@ -55,43 +58,25 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
         # update w through the stochastic gradient update
         w = w - gamma * grad
         # calculate loss
-        loss = compute_loss(y, tx, w)
+        loss = compute_mse(y, tx, w)
         # store w and loss
         ws.append(w)
         losses.append(loss)
     return ws[-1],losses[-1]
 
 def least_squares(y,tx):
-    #Mean tx and y
-    mean_tx = np.mean(tx)
-    mean_y = np.mean(y)
-    n = len(tx)
-    #Calculate the splope and the offset:
-    num = 0
-    den = 0
-    for i in range(n):
-        num += (tx[i]-mean_tx)*(y[i]-y_mean)
-        den += (tx[i]-mean_tx)**2
-    w0 = num/den
-    w1 = mean_y -(w0*mean_tx)
-    w = [w0,w1]
-    loss = compute_loss(y,tx,w)
+    """calculate the least squares solution."""
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    w= np.linalg.solve(a, b)
+    loss = compute_mse(y,tx,w)
     return w,loss
 
-def ridge_regression(y,tx,lambda_):
-    w = []
-    # normal of tx
-    A = tx.T @ tx
-    # Identity matrix with same shape as A
-    I = np.eye(A.shape[0])
-    # 
-    c = tx.T @ y
-    for lambVal in range(1, lambda_+1):
-        # Set up equations Bw = c        
-        lamb_I = lambVal * I
-        B = A + lamb_I
-        # Solve for w
-        w_i = np.linalg.solve(B,c)
-        w.append(w_i)
-    loss = compute_loss(y,tx,w)
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression."""
+    aI = lamb * np.identity(tx.shape[1])
+    a = tx.T.dot(tx) + aI
+    b = tx.T.dot(y)
+    w = np.linalg.solve(a, b)
+    loss = compute_mse(y,tx,w)
     return w,loss
